e334c868080a8fa1a0dd5b78f903ce4876ecb94f
Nik <nn00317@surrey.ac.uk>
2019-10-13 17:44:23 +0100

    Updates

---
+++ precache-manifest.544624c925a8940aef7ec087f6b92243.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d5cb8327a08ff339fdb71a6c13a6b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e70546445709e34416dc",
+    "url": "/static/css/main.9e116a38.chunk.css"
+  },
+  {
+    "revision": "3cb6ad044f017950456c",
+    "url": "/static/js/2.47f78d9c.chunk.js"
+  },
+  {
+    "revision": "e70546445709e34416dc",
+    "url": "/static/js/main.a2874b0c.chunk.js"
+  },
+  {
+    "revision": "117a4f06f3ced80f4a5d",
+    "url": "/static/js/runtime-main.cc944fee.js"
+  },
+  {
+    "revision": "1ca3d27688e0eed8b5e6639a37fafd1a",
+    "url": "/static/media/Assassin.1ca3d276.png"
+  },
+  {
+    "revision": "fac6a01e4b5ecf871f27d50c9058fe6c",
+    "url": "/static/media/Blood-Bound.fac6a01e.png"
+  },
+  {
+    "revision": "e8be173ddca2e2b9317eca18c966fcb2",
+    "url": "/static/media/Brawny.e8be173d.png"
+  },
+  {
+    "revision": "9ecca571eae95fa9dd6543a4e2df3289",
+    "url": "/static/media/Deadeye.9ecca571.png"
+  },
+  {
+    "revision": "e3848045e2c0fca141689a7822cbb257",
+    "url": "/static/media/Demon Hunter.e3848045.png"
+  },
+  {
+    "revision": "55391bf5e0b313aaeaa9f6eaf19123d6",
+    "url": "/static/media/Demon.55391bf5.png"
+  },
+  {
+    "revision": "fc21d52a96269ef7cee4ec6243c3e44c",
+    "url": "/static/media/Dragon.fc21d52a.png"
+  },
+  {
+    "revision": "382589fe44dbf76173bd11df6fb11bf3",
+    "url": "/static/media/Druid.382589fe.png"
+  },
+  {
+    "revision": "8d5055261ec0f6ef32385912416ec076",
+    "url": "/static/media/Elusive.8d505526.png"
+  },
+  {
+    "revision": "37507a252785e750ff959746212a9ada",
+    "url": "/static/media/Heartless.37507a25.png"
+  },
+  {
+    "revision": "babeb67871e166da199fdc575820c020",
+    "url": "/static/media/Human.babeb678.png"
+  },
+  {
+    "revision": "1ccaaf1fa611fd5a4baf7dc3e33accf0",
+    "url": "/static/media/Hunter.1ccaaf1f.png"
+  },
+  {
+    "revision": "eaa128a24267b9aea7e348fafa95f245",
+    "url": "/static/media/Inventor.eaa128a2.png"
+  },
+  {
+    "revision": "c04e1d0995806d92078cadc9ac8a80a3",
+    "url": "/static/media/Knight.c04e1d09.png"
+  },
+  {
+    "revision": "21a4eec8874aca40a649839836898381",
+    "url": "/static/media/Mage.21a4eec8.png"
+  },
+  {
+    "revision": "2f905817e9af2574f13bccac73ad1f76",
+    "url": "/static/media/Primordial.2f905817.png"
+  },
+  {
+    "revision": "aa35cc4ee540e2dce70e58ad8fef133a",
+    "url": "/static/media/Savage.aa35cc4e.png"
+  },
+  {
+    "revision": "39696d2653f93387bc42829bb37c52f1",
+    "url": "/static/media/Scaled.39696d26.png"
+  },
+  {
+    "revision": "2688b99085d94d8e175e8541bc91ef60",
+    "url": "/static/media/Scrappy.2688b990.png"
+  },
+  {
+    "revision": "8b5796f50d064fc19435448ea797411a",
+    "url": "/static/media/Shaman.8b5796f5.png"
+  },
+  {
+    "revision": "6edb74cb9cbcbc06100091d8a6921d3a",
+    "url": "/static/media/Troll.6edb74cb.png"
+  },
+  {
+    "revision": "194e93f39a410f7a9107d5bd52002741",
+    "url": "/static/media/Warlock.194e93f3.png"
+  },
+  {
+    "revision": "0bf219daf5d1ea5aa07c585b5f0c74c8",
+    "url": "/static/media/Warrior.0bf219da.png"
+  }
+]);
