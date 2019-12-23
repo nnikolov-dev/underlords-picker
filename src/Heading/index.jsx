9aa50be1f6df664e45ca7f353d9175874fe3cb89
Nikita Nikolov <nnikolov-post@gmail.com>
2019-12-23 09:02:39 +0000

    Improved mobile

--- src/Heading/index.jsx
+++ src/Heading/index.jsx
@@ -5,7 +5,7 @@ import img from '../assets/images/heading.png'
 const Heading = () => (
 	<div className={styles.Heading} style={{background: `url(${img})`}}>
 		<div className={styles.Text}>
-			<h2>Heading</h2>
+			<h2>Solo Composer</h2>
 		</div>
 	</div>
 )
